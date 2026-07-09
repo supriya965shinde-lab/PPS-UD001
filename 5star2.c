eefff787fe9d6c208875a58c1fd0a3175763b2c8
Supriya shinde <supriya965shinde@gmail.com>
2026-07-09 17:06:40 +0530

    Add program to print right-aligned asterisk triangle
    This program prints a right-aligned triangle of asterisks.

---
+++ 5star2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{ 
+ int i,j,k;
+ for(i=1;i<=5;i++){
+ 
+ 	for(k=5;k>=i;k--){
+ 	printf(" \t");}
+
+    for(j=1;j<=i;j++){ 
+    printf("*\t");
+	} 
+	printf("\n");  
+}
+ return 0;
+}
