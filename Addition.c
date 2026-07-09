9a6417b96500d1954622a6bb18f071f6a2bc4488
Supriya shinde <supriya965shinde@gmail.com>
2026-07-09 17:05:12 +0530

    Add Addition.c to calculate the sum of two numbers

---
+++ Addition.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int num1,num2,sum;
+	
+	printf("enter first number:\t");
+	scanf("%d",&num1);
+	
+	printf("enter second number:\t");
+	scanf("%d",&num2);
+	
+	sum=num1+num2;
+	
+	printf("sum of %d & %d id %d",num1,num2,sum);
+	
+	return 0;
+	
+}
